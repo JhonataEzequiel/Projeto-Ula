1a237342a99847458d6c19533a17fed967763ffd
JhonataEzequiel <57595331+JhonataEzequiel@users.noreply.github.com>
2020-03-20 16:50:19 -0300

    Add files via upload

--- TestBenchUla.v
+++ TestBenchUla.v
@@ -1,85 +1,52 @@
-module TestBenchUla();
+module TestBenchUla;
 reg [3:0]Sel;
-reg [1:0]A, B;
+reg [1:0]A;
+reg B;
 wire [2:0]Saida;
 
-ula u1(.Sel(Sel), .A(A), .B(B), .Saida(Saida));
+ula u1(.Sel(Sel), .A(A), .B(B), .Saida(Saida), .clk());
 
 initial begin
 //Sel = 0000 -> Soma
-		A = 2'b00; B = 1'b0; Sel = 4'b0000; #10;
-		A = 2'b00; B = 1'b1; Sel = 4'b0000; #10; 
-		A = 2'b01; B = 1'b0; Sel = 4'b0000; #10;
-		A = 2'b01; B = 1'b1; Sel = 4'b0000; #10; 
-		A = 2'b10; B = 1'b0; Sel = 4'b0000; #10;
-		A = 2'b10; B = 1'b1; Sel = 4'b0000; #10; 
-		A = 2'b11; B = 1'b0; Sel = 4'b0000; #10;
-		A = 2'b11; B = 1'b1; Sel = 4'b0000; #10; 
+		A = 2'b10; B = 1'b1; Sel = 4'b0000; #10; //2'b10 + 1'b1 = 3'b011 
+		if(Saida != 3'b011) $display("Erro: Falha na soma de 2'b10 + 1'b1");
+		A = 2'b11; B = 1'b1; Sel = 4'b0000; #10; //2'b11 + 1'b1 = 3'b100
+		if(Saida != 3'b100) $display("Erro: Falha na soma de 2'b11 + 1'b1");
 //Sel = 0001 -> Subtração
-		A = 2'b00; B =	1'b0; Sel = 4'b0001; #10; 
-		A = 2'b00; B = 1'b1; Sel = 4'b0001; #10; 
-		A = 2'b01; B = 1'b0; Sel = 4'b0001; #10;
 		A = 2'b01; B = 1'b1; Sel = 4'b0001; #10; 
-		A = 2'b10; B = 1'b0; Sel = 4'b0001; #10;
-		A = 2'b10; B = 1'b1; Sel = 4'b0001; #10; 
-		A = 2'b11; B = 1'b0; Sel = 4'b0001; #10;
-		A = 2'b11; B = 1'b1; Sel = 4'b0001; #10; 
+		if(Saida != 3'b000) $display("Erro: Falha na Subtracao");
 //Sel = 0010 -> Multiplicação
-		A = 2'b00; B = 1'b0; Sel = 4'b0010; #10; 
-		A = 2'b00; B = 1'b1; Sel = 4'b0010; #10; 
-		A = 2'b01; B = 1'b0; Sel = 4'b0010; #10;
-		A = 2'b01; B = 1'b1; Sel = 4'b0010; #10;  
-		A = 2'b10; B = 1'b0; Sel = 4'b0010; #10;
-		A = 2'b10; B = 1'b1; Sel = 4'b0010; #10; 
 		A = 2'b11; B = 1'b0; Sel = 4'b0010; #10;
-		A = 2'b11; B = 1'b1; Sel = 4'b0010; #10; 
+		if(Saida != 3'b000) $display("Erro: Falha na Multiplicacao de 2'b11 * 1'b0");
+		A = 2'b11; B = 1'b1; Sel = 4'b0010; #10;
+		if(Saida != 3'd011) $display("Erro: Falha na Multiplicacao de 2'b11 * 1'b1");
 //Sel = 0011 -> Divisão
-		A = 2'b00; B = 1'b0; Sel = 4'b0011; #10; 
 		A = 2'b00; B = 1'b1; Sel = 4'b0011; #10; 
-		A = 2'b01; B = 1'b0; Sel = 4'b0011; #10;
-		A = 2'b01; B = 1'b1; Sel = 4'b0011; #10; 
-		A = 2'b10; B = 1'b0; Sel = 4'b0011; #10;
+		if(Saida != 3'b000) $display("Erro: Falha na Divisao de 2'b00/1'b1");
 		A = 2'b10; B = 1'b1; Sel = 4'b0011; #10;  
-		A = 2'b11; B = 1'b0; Sel = 4'b0011; #10;
-		A = 2'b11; B = 1'b1; Sel = 4'b0011; #10; 
+		if(Saida != 3'b001) $display("Erro: Falha na Divisao de 2'b10/1'b1");
 //Sel = 0100 -> Deslocamento pra esquerda
-		A = 2'b00; Sel = 4'b0100; #10; 
 		A = 2'b01; Sel = 4'b0100; #10;
-		A = 2'b10; Sel = 4'b0100; #10;
-		A = 2'b11; Sel = 4'b0100; #10;
+		if(Saida != 3'b010) $display("Erro: Deslocamento pela esquerda");
 //Sel = 0101 -> Deslocamento pra direita
-		A = 2'b00; Sel = 4'b0101; #10; 
-		A = 2'b01; Sel = 4'b0101; #10;
 		A = 2'b10; Sel = 4'b0101; #10;
-		A = 2'b11; Sel = 4'b0101; #10;
+		if(Saida != 3'b001) $display("Erro: Deslocamento pela direita");
 //Sel = 0110 -> Rotacionamento pra direita
-		A = 2'b00; Sel = 4'b0110; #10; 
 		A = 2'b01; Sel = 4'b0110; #10;
-		A = 2'b10; Sel = 4'b0110; #10;
-		A = 2'b11; Sel = 4'b0110; #10;
+		if(Saida != 3'b010) $display("Erro: Rotacionamento pela direita");
 //Sel = 0111 -> Rotacionamento pra esquerda
-		A = 2'b00; Sel = 4'b0111; #10; 
-		A = 2'b01; Sel = 4'b0111; #10;
 		A = 2'b10; Sel = 4'b0111; #10;
-		A = 2'b11; Sel = 4'b0111; #10;
+		if(Saida != 3'b001) $display("Erro: Rotacionamento pela esquerda");
 //Sel = 1000 -> And
-		A = 2'b00; B = 1'b0; Sel = 4'b1000; #10; 
 		A = 2'b00; B = 1'b1; Sel = 4'b1000; #10; 
-		A = 2'b01; B = 1'b0; Sel = 4'b1000; #10;
-		A = 2'b01; B = 1'b1; Sel = 4'b1000; #10; 
-		A = 2'b10; B = 1'b0; Sel = 4'b1000; #10;
-		A = 2'b10; B = 1'b1; Sel = 4'b1000; #10; 
-		A = 2'b11; B = 1'b0; Sel = 4'b1000; #10;
+		if(Saida != 0) $display("Erro: And de 2'b00 & 1'b1");
 		A = 2'b11; B = 1'b1; Sel = 4'b1000; #10; 
+		if(Saida != 1) $display("Erro: And de 2'b11 & 1'b1");
 //Sel = 1001 -> Or
 		A = 2'b00; B = 1'b0; Sel = 4'b1001; #10; 
-		A = 2'b00; B = 1'b1; Sel = 4'b1001; #10; 
-		A = 2'b01; B = 1'b0; Sel = 4'b1001; #10;
-		A = 2'b01; B = 1'b1; Sel = 4'b1001; #10; 
-		A = 2'b10; B = 1'b0; Sel = 4'b1001; #10;
-		A = 2'b10; B = 1'b1; Sel = 4'b1001; #10; 
+		if(Saida != 1) $display("Erro: Or de 2'b00 & 1'b0");
 		A = 2'b11; B = 1'b0; Sel = 4'b1001; #10;
-		A = 2'b11; B = 1'b1; Sel = 4'b1001; #10; 
+		if(Saida != 1) $display("Erro: Or de 2'b11 & 1'b0");
 //Sel = 1010 -> Xor
 		A = 2'b00; B = 1'b0; Sel = 4'b1010; #10; 
 		A = 2'b00; B = 1'b1; Sel = 4'b1010; #10; 
--- ula.v
+++ ula.v
@@ -1,20 +1,25 @@
-module ula(Sel, A, B, Saida);
+module ula(Sel, A, B, Saida, clk);
+input clk;
 input [1:0]A; //Para que haja deslocamento de bits e rotacionamento, o A precisa ter mais que 1 bit
 input B; 
-output reg [2:0]Saida; //O maior número que a saída irá assumir será 2'b11, resultante da expressão "2'b11 * 1'b1"
-input [3:0]Sel;
+output reg [2:0]Saida; //O maior número que a saída irá assumir será 3'b100, resultante da expressão "2'b11 + 1'b1"
+input [3:0]Sel; //Chave Seletora
+
 reg [1:0]auxiliar;
 
 wire And1, Xor1, Or1;
 
-and (And1, A, B); xor (Xor1, A, B); or (Or1, A, B);
+and (And1, A[1:0], B); xor (Xor1, A, B); or (Or1, A, B);
 
-always@(*)begin
+always@(posedge clk)begin
 	case (Sel)
-		4'b0000:Saida <= (A + B); //Soma
-		
+		4'b0000:Saida <= {1'b0, A} + {1'b0, B}; //Soma
+		/*
+			é preciso concatenar um bit antes das entradas, para que o Carry Out de 2'b11 + 1'b1
+			seja adicionado a expressão
+		*/
 		4'b0001:Saida <= (A - B);//Subtração
-
+		
 		4'b0010:Saida <= (A * B);//Multiplicação
 		
 		4'b0011:Saida <= (A / B);//Divisão
@@ -24,12 +29,12 @@ always@(*)begin
 		4'b0101:Saida <= (A >> 1);//Deslocamento pra direita
 		
 		4'b0110:begin
-			auxiliar = {A[0], A[1]};//Rotacionamento pra direita, é igual pq tem so 2 bits
+			auxiliar = {A[0], A[1]};//Rotacionamento pra direita
 			Saida <= auxiliar;
 			end
 		
 		4'b0111:begin
-			auxiliar = {A[0], A[1]};//Rotacionamento pra esquerda
+			auxiliar = {A[0], A[1]};//Rotacionamento pra esquerda, é igual pq A tem apenas 2 bits
 			Saida <= auxiliar;
 			end
 		
@@ -45,7 +50,7 @@ always@(*)begin
 			
 		4'b1101:Saida <= ~Xor1;//NXor
 			
-		4'b1110: 
+		4'b1110: //A > B
 			begin
 				if(A > B) begin
 					Saida <= 1;
@@ -54,7 +59,7 @@ always@(*)begin
 					Saida <= 0;
 				end
 			end
-		4'b1111: 
+		4'b1111: //A == B
 			begin
 				if(A == B) begin
 					Saida <= 1;
